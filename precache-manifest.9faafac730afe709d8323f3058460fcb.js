120fa475d378343f1758683b0f15af364c8868fb
Thacio Oliveira <thacio.oliveira_ext@glintt.com>
2020-04-10 10:19:22 +0100

    Updates

---
+++ precache-manifest.9faafac730afe709d8323f3058460fcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286d82e44baa49aa8499c61fca8e7c7d",
+    "url": "/react-course/index.html"
+  },
+  {
+    "revision": "2736c57e23359156c628",
+    "url": "/react-course/static/css/main.0dfc2579.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/react-course/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-course/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2736c57e23359156c628",
+    "url": "/react-course/static/js/main.7ee2a760.chunk.js"
+  },
+  {
+    "revision": "6323d1a8e46e47a1cbd7",
+    "url": "/react-course/static/js/runtime-main.157c53d9.js"
+  }
+]);
--- src/components/search-box/search-box.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './search-box.styles.css'
-
-export const SearchBox = ({placeHolder, handleChange}) => (
-    <input type="search" 
-           className='search'
-           placeholder={placeHolder}
-           onChange={handleChange}></input>
-);
